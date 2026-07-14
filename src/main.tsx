38e533b17802c0dc82749a4288ee4f9c460945d1
Grouchy-Lawn-Chair <mitchell.weiner@dhcs.ca.gov>
2026-07-13 22:59:27 -0700

    Use React recipe physics integration root

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import RecipeAppIntegration from './RecipeAppIntegration';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RecipeAppIntegration />
   </StrictMode>
 );
