240146cc9279d807f90c224faed62ac13434581e
Grouchy-Lawn-Chair <mitchell.weiner@dhcs.ca.gov>
2026-07-14 22:56:43 -0700

    Keep selected recipe when reopening a zone

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import RecipeAppIntegration from './RecipeAppIntegration';
+import RecipeSelectionPersistence from './RecipeSelectionPersistence';
 import { installRecipeEdgeFallback } from './utils/recipeEdgeFallback';
 import './index.css';
 
@@ -8,6 +9,7 @@ installRecipeEdgeFallback();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <RecipeSelectionPersistence />
     <RecipeAppIntegration />
   </StrictMode>,
 );
