9956b19679fc7009093723efb6c1196315658027
Grouchy-Lawn-Chair <mitchell.weiner@dhcs.ca.gov>
2026-07-14 22:47:10 -0700

    Allow recipe generation without manually marked edges

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import RecipeAppIntegration from './RecipeAppIntegration';
+import { installRecipeEdgeFallback } from './utils/recipeEdgeFallback';
 import './index.css';
+
+installRecipeEdgeFallback();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RecipeAppIntegration />
-  </StrictMode>
+  </StrictMode>,
 );
